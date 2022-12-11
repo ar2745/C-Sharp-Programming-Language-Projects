84b7dee1754b43d66e859097846a9cb0da23efaf
ar2745 <76749119+ar2745@users.noreply.github.com>
2022-12-10 23:47:39 -0500

    Add files via upload

---
+++ Continue Statement/Continue Statement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+
+            while(x <= 50)
+            {
+                if(x >= 20 && x <= 35)
+                {
+                    x++;
+                    continue;
+                }
+                Console.WriteLine("The new value of x is: {0}", x);
+                x++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Switch Statement/Switch Statement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter student's grade: ");
+            char score = Convert.ToChar(Console.Read());
+
+            switch (score)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+                case 'B':
+                    Console.WriteLine("Good");
+                    break;
+                case 'C':
+                    Console.WriteLine("Fair");
+                    break;
+                case 'D':
+                    Console.WriteLine("Poor");
+                    break;
+                case 'F':
+                    Console.WriteLine("Fail");
+                    break;
+                default:
+                    Console.WriteLine("Invalid grade");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
