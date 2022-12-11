59e6973eaff458b8e3faddf6ce058f0b75e60181
ar2745 <76749119+ar2745@users.noreply.github.com>
2022-12-10 23:40:24 -0500

    Add files via upload

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namef = "Paul";
+            int x = 12;
+            Console.WriteLine(namef + " is " + x + " years old");
+            Console.WriteLine("The name of " + namef + " school is Academy");
+            Console.WriteLine(namef + " loves to play soccer");
+            Console.WriteLine(namef + " loves C# programming language");
+
+            Console.ReadKey();
+        }
+    }
+}
