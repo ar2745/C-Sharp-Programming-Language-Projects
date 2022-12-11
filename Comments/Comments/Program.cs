482e7a966fc49d0c662b24086df883a1a95ce662
ar2745 <76749119+ar2745@users.noreply.github.com>
2022-12-10 23:41:18 -0500

    Add files via upload

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is single line comments
+            /*This
+             * is
+             * multiple
+             * line
+             * comment
+            */
+
+            Console.WriteLine("This is C# programming"); //C# Code
+            Console.WriteLine("This is Java programming"); //Java Code
+            Console.WriteLine("This is C programming"); //C Code
+            Console.WriteLine("This is C++ programming"); //C++ Code
+            Console.WriteLine("This is Python programming"); //Python Code
+            Console.ReadKey();
+        }
+    }
+}
---
+++ User Input/User Input/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            //First example
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            
+            Console.Write("What is your age? ");
+            string age = Console.ReadLine();
+
+            Console.Write("What is your phone number? ");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name + " and your age is " + age + " and your phone number is " + phone);
+            
+            //Second example
+            Console.Write("Enter a first number ");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a second number ");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a third number ");
+            double num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 * num2 * num3);
+
+            Console.ReadKey();
+        }
+    }
+}
