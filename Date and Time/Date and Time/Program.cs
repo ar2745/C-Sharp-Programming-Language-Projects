9201f1c41899ea48a3ddd3cff5a723968a189c1c
ar2745 <76749119+ar2745@users.noreply.github.com>
2022-12-10 23:43:11 -0500

    Add files via upload

---
+++ Date and Time/Date and Time/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_and_Time
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            DateTime DT = new DateTime(2022, 09, 10, 13, 52, 12);
+            string printdt = string.Format("The time is {0:t} on today {0:D}", DT);
+
+            Console.WriteLine(printdt);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Typecasting/Typecasting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Type_Conversions
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            // First example
+            Console.WriteLine("Enter a number ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            
+            //Second example
+            Console.WriteLine("Enter a number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 / num2);
+
+            Console.ReadKey();
+        }
+    }
+}
