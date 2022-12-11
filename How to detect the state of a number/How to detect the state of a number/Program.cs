503b901f274877741f24fc36a25457d85ffaa382
ar2745 <76749119+ar2745@users.noreply.github.com>
2022-12-11 00:16:19 -0500

    Add files via upload

---
+++ How to detect the state of a number/How to detect the state of a number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_Of_A_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            Console.Write("Enter some number: ");
+            x = int.Parse(Console.ReadLine());
+
+            if(x > 0)
+            {
+                Console.Write("This is a positive number");
+            }
+            else if(x == 0)
+            {
+                Console.Write("This is a zero");
+            }
+            else
+            {
+                Console.Write("This is a negative number");
+            }
+        }
+    }
+}
---
+++ Minimum and maximum number in an array/Minimum and maximum number in an array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_And_Maximum_Number_In_An_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 4, 78, 98, 3, 0, 3, 12, 78, 95, 32, 67 };
+
+            Console.WriteLine("The maximum number = " + arr1.Max());
+            Console.WriteLine("The minimum number = " + arr1.Min());
+        }
+    }
+} 
