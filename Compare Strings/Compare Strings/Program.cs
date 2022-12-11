02ad155c92c45ffeedd0a6b82c96d3b3ebd228de
ar2745 <76749119+ar2745@users.noreply.github.com>
2022-12-10 23:54:29 -0500

    Add files via upload

---
+++ Compare Strings/Compare Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparing_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Peter is good";
+            string lastName = "Paul is best";
+
+            if(String.Compare(firstName, lastName) == 0)
+            {
+                Console.WriteLine(firstName + " and " + lastName + " are equal");
+            }
+            else
+            {
+                Console.WriteLine(firstName + " and " + lastName + " are not equal");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Get A Substring/Get A Substring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_A_Subtstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "This is a sub string of Mr. Peter";
+            string subname = name.Substring(24);
+
+            Console.WriteLine(name);
+            Console.WriteLine(subname);
+
+            Console.ReadKey();
+        }
+    }
+}
