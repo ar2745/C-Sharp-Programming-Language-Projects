65c0080c06b579823dc6850676720833b16c6b60
ar2745 <76749119+ar2745@users.noreply.github.com>
2022-12-10 23:48:32 -0500

    Add files via upload

---
+++ For Loop/For Loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            
+            for(x = 1; x <= 50; x++)
+            {
+                Console.WriteLine("The new value of x  is: {0} ", x);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Nested For Loop/Nested For Loop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            for(x = 2; x < 50; x++)
+            {
+                for (y = 2; y <= (x / y); y++)
+                {
+                    if ((x % y) == 0)
+                    {
+                        break;
+                    }
+                }
+                if(y > (x/y))
+                {
+                    Console.WriteLine(x + " is a prime number");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
